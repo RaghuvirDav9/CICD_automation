a285c31cfea57f566d709f5b095521cc3a0079ae
Raghuvir Dav <davraghuvir9@gmail.com>
2021-03-31 14:29:05 +0530

    app

--- demo.sh
+++
@@ -1,4 +0,0 @@
-a=$(<appspec.yaml)
-OUTPUT=$(aws ecs describe-task-definition --task-definition TaskDefinitionDEMO | jq '.[] | .taskDefinitionArn')
-echo "${a/task-def/$OUTPUT}"
-echo "${a/task-def/$OUTPUT}" > appspec.yaml
